cd997e28529cdf983c1f550977ebba68e82d46fe
Kyung Lee <kyung.lee.official@outlook.com>
2023-04-22 09:45:02 +0800

    Update Footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,7 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { getUrlLocale, useLanguageStore } from "stores/language";
-import { Document, Community, More } from "../icons/Icons";
+import {
+	Document,
+	Community,
+	More,
+	FluentUiDocument,
+	QuestionCircle,
+	DiscordAlt,
+	FacebookCircle,
+	Twitter,
+	Instagram,
+	Youtube,
+	Verified,
+	Store,
+	EmailOutline,
+} from "../icons/Icons";
 
 const ColTitle = (props: any) => {
 	const { children } = props;
@@ -18,7 +32,11 @@ const ColTitle = (props: any) => {
 const Link = (props: any) => {
 	const { href, target, children } = props;
 	return (
-		<a href={href} target={target} className="hover:text-sky-500">
+		<a
+			href={href}
+			target={target}
+			className="flex items-center gap-2 hover:text-sky-500"
+		>
 			{children}
 		</a>
 	);
@@ -27,7 +45,7 @@ const Link = (props: any) => {
 const FooterCol = (props: any) => {
 	const { children } = props;
 	return (
-		<div className="flex flex-col justify-start xl:justify-center items-start w-40 mb-6 xl:m-0 gap-2">
+		<div className="flex flex-col justify-start xl:justify-center items-start w-44 mb-6 xl:m-0 gap-2">
 			{children}
 		</div>
 	);
@@ -51,73 +69,83 @@ const Footer = () => {
 			<div className="flex flex-col xl:flex-row justify-start xl:justify-center items-center xl:items-start pb-2 xl:py-8 xl:gap-28">
 				<FooterCol>
 					<ColTitle>
-						<Document size={"36"} />
+						<Document size={36} />
 						{t("footer.relevantDocs")}
 					</ColTitle>
 					<Link
 						href={`/${urlLocale}/docs/chitubox-basic/latest/introduction`}
 					>
+						<FluentUiDocument size={24} />
 						{t("footer.chituboxBasicDoc")}
 					</Link>
 					<Link
 						href={`/${urlLocale}/docs/chitubox-pro/latest/introduction`}
 					>
+						<FluentUiDocument size={24} />
 						{t("footer.chituboxProDoc")}
 					</Link>
 					<Link
 						href={`/${urlLocale}/docs/faq/latest/chitubox-basic-faq`}
 					>
+						<QuestionCircle size={24} />
 						{t("footer.faqDoc")}
 					</Link>
 				</FooterCol>
 				<FooterCol>
 					<ColTitle>
-						<Community size={"36"} />
+						<Community size={36} />
 						{t("footer.community")}
 					</ColTitle>
 					<Link
 						target={"_blank"}
 						href={t("footer.DiscordLink") as string}
 					>
+						<DiscordAlt size={24} />
 						{t("footer.Discord")}
 					</Link>
 					<Link
 						target={"_blank"}
 						href={t("footer.facebookLink") as string}
 					>
+						<FacebookCircle size={24} />
 						{t("footer.facebook")}
 					</Link>
 					<Link
 						target={"_blank"}
 						href={t("footer.twitterLink") as string}
 					>
+						<Twitter size={24} />
 						{t("footer.twitter")}
 					</Link>
 					<Link
 						target={"_blank"}
 						href={t("footer.instagramLink") as string}
 					>
+						<Instagram size={24} />
 						{t("footer.instagram")}
 					</Link>
 					<Link
 						target={"_blank"}
 						href={t("footer.youtubeLink") as string}
 					>
+						<Youtube size={24} />
 						{t("footer.youtube")}
 					</Link>
 				</FooterCol>
 				<FooterCol>
 					<ColTitle>
-						<More size={"36"} />
+						<More size={36} />
 						{t("footer.more")}
 					</ColTitle>
 					<Link
 						target={"_blank"}
 						href={t("footer.chituboxSiteLink") as string}
 					>
+						<Verified size={24} />
 						{t("footer.chituboxSite")}
 					</Link>
 					<Link href={t("footer.supportEmailLink") as string}>
+						<EmailOutline size={24} />
 						{t("footer.supportEmail")}
 					</Link>
 					{language === "en_US" ? (
@@ -125,6 +153,7 @@ const Footer = () => {
 							target={"_blank"}
 							href={t("footer.chituSystemsLink") as string}
 						>
+							<Store size={24} />
 							{t("footer.chituSystems")}
 						</Link>
 					) : null}
