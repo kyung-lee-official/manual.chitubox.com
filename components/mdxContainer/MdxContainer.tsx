8fc8e20df3a1c46465dbfd199a5ed6e077666514
Li Geng <ligeng@cbd-3d.com>
2023-05-11 16:04:49 +0800

    Update: add NotLatestHint.tsx
    * Add NotLatestHint.tsx
    * Update requirements.mdx

--- components/mdxContainer/MdxContainer.tsx
+++ components/mdxContainer/MdxContainer.tsx
@@ -3,16 +3,18 @@ import React, { useContext } from "react";
 import { DocContext } from "../docsLayout/DocContext";
 import { VersionTag } from "../versionTag/VersionTag";
 import { MDXComponent } from "../mdxComponent/MDXComponent";
+import { useRouter } from "next/router";
+import { NotLatestHint } from "../notLatestHint/NotLatestHint";
 
 export const MdxContainer: React.FC<any> = (props) => {
 	const { children } = props;
-	const {
-		docInstanceContext: { isVersioned },
-	} = useContext(DocContext);
+	const { docInstanceContext } = useContext(DocContext);
+	const router = useRouter();
 
 	return (
 		<div className="flex-auto min-w-0 px-12 py-8">
-			{isVersioned && <VersionTag />}
+			{router.pathname.split("/")[4] !== "latest" && <NotLatestHint />}
+			{docInstanceContext.isVersioned && <VersionTag />}
 			<MDXProvider components={MDXComponent}>{children}</MDXProvider>
 		</div>
 	);
