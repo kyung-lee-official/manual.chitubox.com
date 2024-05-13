badce0c424a03d9e69112576bf83ad078ea0ea57
Kyung Lee <kyung.lee.official@outlook.com>
2024-05-13 20:18:03 +0800

    fix: switch redirection from client side to server side

--- src/app/en-US/docs/chitubox-basic/latest/introduction/page.tsx
+++ src/app/en-US/docs/chitubox-basic/latest/introduction/page.tsx
@@ -1,27 +1,18 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import { redirect } from "@/navigation";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 export default function Page() {
+	unstable_setRequestLocale("en-US");
 
-	const router = useRouter();
-	useEffect(() => {
-		const interval = setInterval(() => {
-			router.push("/en-US/chitubox-basic/latest/introduction");
-		}, 1000);
-		return () => {
-			clearInterval(interval);
-		};
-	}, [router]);
+	redirect("/chitubox-basic/latest/introduction");
+
 	return (
 		<div
 			className="flex justify-center items-center h-70vh
-                text-2xl font-bold text-gray-500 dark:text-gray-400 
-                bg-gray-100 dark:bg-gray-800"
+			text-2xl font-bold text-gray-500 dark:text-gray-400 
+			bg-gray-100 dark:bg-gray-800"
 		>
 			Redirecting...
 		</div>
 	);
-
 }
