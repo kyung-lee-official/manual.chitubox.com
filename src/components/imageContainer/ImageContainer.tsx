c15b8152fddf9aa4695dea63f7bdabe8015e9391
Kyung Lee <kyung.lee.official@outlook.com>
2024-11-22 17:31:17 +0800

    fix: ImageContainer responsive issue

--- src/components/imageContainer/ImageContainer.tsx
+++ src/components/imageContainer/ImageContainer.tsx
@@ -2,8 +2,8 @@ export const ImageContainer = (props: any) => {
 	const { src, alt, style } = props;
 	return (
 		<div
-			className={`flex items-center w-fit my-4
-            drop-shadow rounded-md overflow-hidden`}
+			className={`flex items-center w-fit max-w-full my-4
+            drop-shadow rounded-md overflow-scroll`}
 		>
 			<img src={src} alt={alt ?? ""} style={style} />
 		</div>
