19ddd4fcda4666078483b76de8b637dff29fd6d4
Kyung Lee <kyung.lee.official@outlook.com>
2025-03-10 09:40:51 +0800

    docs: remove ubuntu support temporarily

--- src/docComponents/systemRequirements/Pro2Requirements.tsx
+++ src/docComponents/systemRequirements/Pro2Requirements.tsx
@@ -119,62 +119,62 @@ const enItems = [
 			</ResponsiveTable>
 		),
 	},
-	{
-		key: "ubuntu",
-		label: (
-			<Label>
-				<div>Ubuntu</div>
-				<UbuntuIcon size={"24px"} />
-			</Label>
-		),
-		children: (
-			<ResponsiveTable>
-				<thead>
-					<tr>
-						<th>Specification</th>
-						<th>Minimum</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>CPU</td>
-						<td>Intel® Core™ i5 6600K or AMD Ryzen™ 5 1600</td>
-					</tr>
-					<tr>
-						<td>OS</td>
-						<td>
-							Ubuntu 20.04 LTS <br /> Ubuntu 22.04 LTS <br />
-							Ubuntu 24.04 LTS
-						</td>
-					</tr>
-					<tr>
-						<td>RAM</td>
-						<td>16GB</td>
-					</tr>
-					<tr>
-						<td>Hard Drive</td>
-						<td>At least 10GB of Storage Space</td>
-					</tr>
-					<tr>
-						<td>GPU</td>
-						<td>GPU RAM: ≥1GB</td>
-					</tr>
-					<tr>
-						<td>Display Resolution</td>
-						<td>1920 × 1080</td>
-					</tr>
-				</tbody>
-			</ResponsiveTable>
-			// <div
-			// 	className="flex justify-center items-center p-4 my-8
-			// 	text-neutral-500 text-2xl
-			// 	border border-neutral-200
-			// 	rounded-md"
-			// >
-			// 	Ubuntu version is coming soon. Stay tuned...
-			// </div>
-		),
-	},
+	// {
+	// 	key: "ubuntu",
+	// 	label: (
+	// 		<Label>
+	// 			<div>Ubuntu</div>
+	// 			<UbuntuIcon size={"24px"} />
+	// 		</Label>
+	// 	),
+	// 	children: (
+	// 		<ResponsiveTable>
+	// 			<thead>
+	// 				<tr>
+	// 					<th>Specification</th>
+	// 					<th>Minimum</th>
+	// 				</tr>
+	// 			</thead>
+	// 			<tbody>
+	// 				<tr>
+	// 					<td>CPU</td>
+	// 					<td>Intel® Core™ i5 6600K or AMD Ryzen™ 5 1600</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td>OS</td>
+	// 					<td>
+	// 						Ubuntu 20.04 LTS <br /> Ubuntu 22.04 LTS <br />
+	// 						Ubuntu 24.04 LTS
+	// 					</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td>RAM</td>
+	// 					<td>16GB</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td>Hard Drive</td>
+	// 					<td>At least 10GB of Storage Space</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td>GPU</td>
+	// 					<td>GPU RAM: ≥1GB</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td>Display Resolution</td>
+	// 					<td>1920 × 1080</td>
+	// 				</tr>
+	// 			</tbody>
+	// 		</ResponsiveTable>
+	// 		// <div
+	// 		// 	className="flex justify-center items-center p-4 my-8
+	// 		// 	text-neutral-500 text-2xl
+	// 		// 	border border-neutral-200
+	// 		// 	rounded-md"
+	// 		// >
+	// 		// 	Ubuntu version is coming soon. Stay tuned...
+	// 		// </div>
+	// 	),
+	// },
 ];
 
 const zhItems = [
@@ -284,59 +284,59 @@ const zhItems = [
 			</ResponsiveTable>
 		),
 	},
-	{
-		key: "ubuntu",
-		label: (
-			<Label>
-				<div>Ubuntu</div>
-				<UbuntuIcon size={"24px"} />
-			</Label>
-		),
-		children: (
-			<ResponsiveTable>
-				<thead>
-					<tr>
-						<th>规格</th>
-						<th>最低配置</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>处理器</td>
-						<td>Intel® Core™ i5 6600K 或 AMD Ryzen™ 5 1600</td>
-					</tr>
-					<tr>
-						<td>操作系统</td>
-						<td>Ubuntu 20.04 LTS <br/> Ubuntu 22.04 LTS <br/> Ubuntu 24.04 LTS</td>
-					</tr>
-					<tr>
-						<td>内存</td>
-						<td>16GB</td>
-					</tr>
-					<tr>
-						<td>硬盘</td>
-						<td>不低于 10GB 的硬盘存储空间</td>
-					</tr>
-					<tr>
-						<td>显卡</td>
-						<td>GPU 显存: ≥1GB</td>
-					</tr>
-					<tr>
-						<td>显示器分辨率</td>
-						<td>1920 × 1080</td>
-					</tr>
-				</tbody>
-			</ResponsiveTable>
-			// <div
-			// 	className="flex justify-center items-center p-4 my-8
-			// 	text-neutral-500 text-2xl
-			// 	border border-neutral-200
-			// 	rounded-md"
-			// >
-			// 	Ubuntu 版本正在开发中，敬请期待...
-			// </div>
-		),
-	},
+	// {
+	// 	key: "ubuntu",
+	// 	label: (
+	// 		<Label>
+	// 			<div>Ubuntu</div>
+	// 			<UbuntuIcon size={"24px"} />
+	// 		</Label>
+	// 	),
+	// 	children: (
+	// 		<ResponsiveTable>
+	// 			<thead>
+	// 				<tr>
+	// 					<th>规格</th>
+	// 					<th>最低配置</th>
+	// 				</tr>
+	// 			</thead>
+	// 			<tbody>
+	// 				<tr>
+	// 					<td>处理器</td>
+	// 					<td>Intel® Core™ i5 6600K 或 AMD Ryzen™ 5 1600</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td>操作系统</td>
+	// 					<td>Ubuntu 20.04 LTS <br/> Ubuntu 22.04 LTS <br/> Ubuntu 24.04 LTS</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td>内存</td>
+	// 					<td>16GB</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td>硬盘</td>
+	// 					<td>不低于 10GB 的硬盘存储空间</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td>显卡</td>
+	// 					<td>GPU 显存: ≥1GB</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td>显示器分辨率</td>
+	// 					<td>1920 × 1080</td>
+	// 				</tr>
+	// 			</tbody>
+	// 		</ResponsiveTable>
+	// 		// <div
+	// 		// 	className="flex justify-center items-center p-4 my-8
+	// 		// 	text-neutral-500 text-2xl
+	// 		// 	border border-neutral-200
+	// 		// 	rounded-md"
+	// 		// >
+	// 		// 	Ubuntu 版本正在开发中，敬请期待...
+	// 		// </div>
+	// 	),
+	// },
 ];
 
 const zh_twItems = [
@@ -418,59 +418,59 @@ const zh_twItems = [
 			</ResponsiveTable>
 		),
 	},
-	{
-		key: "ubuntu",
-		label: (
-			<Label>
-				<div>Ubuntu</div>
-				<UbuntuIcon size={"24px"} />
-			</Label>
-		),
-		children: (
-			// <ResponsiveTable>
-			// 	<thead>
-			// 		<tr>
-			// 			<th>規格</th>
-			// 			<th>最低配置</th>
-			// 		</tr>
-			// 	</thead>
-			// 	<tbody>
-			// 		<tr>
-			// 			<td>處理器</td>
-			// 			<td>Intel® Core™ i5 6600K 或 AMD Ryzen™ 5 1600</td>
-			// 		</tr>
-			// 		<tr>
-			// 			<td>操作系統</td>
-			// 			<td>Ubuntu 20.04 LTS</td>
-			// 		</tr>
-			// 		<tr>
-			// 			<td>内存</td>
-			// 			<td>4GB</td>
-			// 		</tr>
-			// 		<tr>
-			// 			<td>硬盤</td>
-			// 			<td>不低於 10GB 的硬盤存儲空間</td>
-			// 		</tr>
-			// 		<tr>
-			// 			<td>顯卡</td>
-			// 			<td>GPU 顯存: ≥1GB</td>
-			// 		</tr>
-			// 		<tr>
-			// 			<td>顯示器分辨率</td>
-			// 			<td>≥ 1280x960</td>
-			// 		</tr>
-			// 	</tbody>
-			// </ResponsiveTable>
-			<div
-				className="flex justify-center items-center p-4 my-8
-				text-neutral-500 text-2xl
-				border border-neutral-200
-				rounded-md"
-			>
-				Ubuntu 版本正在開發中，敬請期待...
-			</div>
-		),
-	},
+	// {
+	// 	key: "ubuntu",
+	// 	label: (
+	// 		<Label>
+	// 			<div>Ubuntu</div>
+	// 			<UbuntuIcon size={"24px"} />
+	// 		</Label>
+	// 	),
+	// 	children: (
+	// 		// <ResponsiveTable>
+	// 		// 	<thead>
+	// 		// 		<tr>
+	// 		// 			<th>規格</th>
+	// 		// 			<th>最低配置</th>
+	// 		// 		</tr>
+	// 		// 	</thead>
+	// 		// 	<tbody>
+	// 		// 		<tr>
+	// 		// 			<td>處理器</td>
+	// 		// 			<td>Intel® Core™ i5 6600K 或 AMD Ryzen™ 5 1600</td>
+	// 		// 		</tr>
+	// 		// 		<tr>
+	// 		// 			<td>操作系統</td>
+	// 		// 			<td>Ubuntu 20.04 LTS</td>
+	// 		// 		</tr>
+	// 		// 		<tr>
+	// 		// 			<td>内存</td>
+	// 		// 			<td>4GB</td>
+	// 		// 		</tr>
+	// 		// 		<tr>
+	// 		// 			<td>硬盤</td>
+	// 		// 			<td>不低於 10GB 的硬盤存儲空間</td>
+	// 		// 		</tr>
+	// 		// 		<tr>
+	// 		// 			<td>顯卡</td>
+	// 		// 			<td>GPU 顯存: ≥1GB</td>
+	// 		// 		</tr>
+	// 		// 		<tr>
+	// 		// 			<td>顯示器分辨率</td>
+	// 		// 			<td>≥ 1280x960</td>
+	// 		// 		</tr>
+	// 		// 	</tbody>
+	// 		// </ResponsiveTable>
+	// 		<div
+	// 			className="flex justify-center items-center p-4 my-8
+	// 			text-neutral-500 text-2xl
+	// 			border border-neutral-200
+	// 			rounded-md"
+	// 		>
+	// 			Ubuntu 版本正在開發中，敬請期待...
+	// 		</div>
+	// 	),
+	// },
 ];
 
 const onChange = (key: string) => {};
